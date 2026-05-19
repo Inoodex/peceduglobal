0a25d8aa8c9720de6fd9a1f8aec2fa1c13f85b29
juel-47 <juel@inoodex.com>
2026-05-19 12:29:45 +0600

    build(assets): rebuild production assets
    Rebuild all frontend assets to reflect recent changes in components, styles, and dependencies. This includes updating the manifest and replacing all compiled JS and CSS files in the public build directory.

--- public/build/assets/check-HVD4TSF8.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./info-j0AHI-1T.js";var t=e(`check`,[[`path`,{d:`M20 6 9 17l-5-5`,key:`1gmf2c`}]]);export{t};
--- public/build/assets/chevron-up-DR2pMAqJ.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./info-j0AHI-1T.js";var t=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t};
