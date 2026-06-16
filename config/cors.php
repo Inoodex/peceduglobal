4cd3df1295ad132696be057c485bbe9a8f556e94
juel-47 <juel@inoodex.com>
2026-06-16 13:54:16 +0600

    feat(cors): add additional allowed origins for local development

--- config/cors.php
+++ config/cors.php
@@ -21,9 +21,12 @@ return [
 
     'allowed_origins' => [
         'https://peceduglobal.com',
+        'https://www.peceduglobal.com',
         'https://apps.peceduglobal.com',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://localhost:3001',
+        'http://127.0.0.1:3001',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
     ],
