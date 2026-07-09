2300c69797eb2a9ce2190178313d91364f5295ab
juel-47 <juel@inoodex.com>
2026-07-09 09:56:42 +0600

    refactor: remove soft deletes from BlogPost model and migration

--- app/Models/BlogPost.php
+++ app/Models/BlogPost.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BlogPost extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'author_id',
